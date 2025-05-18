52c1c3863d1f5b01ede0878d60adbc4376cfa238
haritha037 <e20037@eng.pdn.ac.lk>
2025-05-18 20:44:51 +0530

    Implemented the commands-called from the front end and sent to the mqtt broker

---
+++ gait-analysis-backend/src/main/java/com/_yp/gaitMate/dto/command/CommandRequestDto.java
@@ -0,0 +1,23 @@
+package com._yp.gaitMate.dto.command;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * DTO representing a command request from the frontend to be sent via MQTT.
+ */
+@Getter
+@Setter
+public class CommandRequestDto {
+
+    @NotBlank(message = "Command must not be blank")
+    private String command;
+
+    public enum CommandType {
+        CHECK_CALIBRATION,
+        START_CALIBRATION,
+        CAPTURE_ORIENTATION,
+        BEGIN_STREAMING
+    }
+}
