1efecca716be24f28a5c9fc5542f2880c7d2c535
Chamodi Senaratne <e20365@eng.pdn.ac.lk>
2025-05-26 22:07:39 +0530

    Update rootServices.js

--- gait-analysis-frontend/src/services/rootServices.js
+++ gait-analysis-frontend/src/services/rootServices.js
@@ -1,12 +1,12 @@
-import axios from './axiosInstance';
+import axiosInstance from './axiosInstance.js';
 
-export const getClinics = () => axios.get('/clinics');
-export const addClinic = (data) => axios.post('/clinics', data);
+export const getClinics = () => axiosInstance.get('/clinics');
+export const addClinic = (data) => axiosInstance.post('/clinics', data);
 
-export const getSensorKits = () => axios.get('/sensor-kits?status=IN_STOCK');
+export const getSensorKits = () => axiosInstance.get('/sensor-kits?status=IN_STOCK');
 export const getSensorKitsByClinic = (clinicId) =>
-  axios.get(`/sensor-kits?clinic_id=${clinicId}`);
-export const addSensorKit = (data) => axios.post('/sensor-kits', data);
+  axiosInstance.get(`/sensor-kits?clinic_id=${clinicId}`);
+export const addSensorKit = (data) => axiosInstance.post('/sensor-kits', data);
 
 export const assignSensorKits = (clinicId, sensorKitIds) =>
-  axios.post('/sensor-kits/assign-to-clinic', { clinicId, sensorKitIds });
+  axiosInstance.post('/sensor-kits/assign-to-clinic', { clinicId, sensorKitIds });
