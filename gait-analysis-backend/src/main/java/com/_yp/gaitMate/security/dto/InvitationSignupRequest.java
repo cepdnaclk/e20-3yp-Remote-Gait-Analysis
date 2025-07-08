4a8f3efd6174cdc1b7b4c99302f811aca6e348a9
haritha037 <e20037@eng.pdn.ac.lk>
2025-07-08 18:41:27 +0530

    Created the InvitationSignupRequest dto

---
+++ gait-analysis-backend/src/main/java/com/_yp/gaitMate/security/dto/InvitationSignupRequest.java
@@ -0,0 +1,29 @@
+package com._yp.gaitMate.security.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class InvitationSignupRequest {
+
+    @NotBlank(message = "Invitation token is required")
+    private String token;
+
+    @Email(message = "Valid email is required")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
+    private String username;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
+    private String password;
+}
