754ba9d0f4faeb9f6c989d9501fa6f4718f2404d
Yohan Senanayake <yohansenanayake4321@gmail.com>
2025-09-23 07:05:33 +0530

    EngEx 7.00 AM

--- POC/WifiNode/src/main.cpp
+++ POC/WifiNode/src/main.cpp
@@ -10,7 +10,8 @@
 // Configuration
 // const char* ssid = "Yohan's Galaxy A52";
 // const char* password = "11111111";
-const char* mqtt_server = "a18e6b70jffugd-ats.iot.eu-north-1.amazonaws.com";
+//const char* mqtt_server = "a18e6b70jffugd-ats.iot.eu-north-1.amazonaws.com";
+const char* mqtt_server = "aku8nka5mp21i-ats.iot.us-east-1.amazonaws.com";
 const String DEVICE_ID = "601";
 
 bool stopStreaming = true;
@@ -60,57 +61,58 @@ rqXRfboQnoZsG4q5WTP468SQvvG5
 
 const char AWS_CERT_CRT[] = R"EOF(
 -----BEGIN CERTIFICATE-----
-MIIDWTCCAkGgAwIBAgIUM2xti76vd4XX3NFM/RIJQv0hpeIwDQYJKoZIhvcNAQEL
+MIIDWTCCAkGgAwIBAgIUTvi5CUrmnjPuQ11WxHS9NXb7Yi0wDQYJKoZIhvcNAQEL
 BQAwTTFLMEkGA1UECwxCQW1hem9uIFdlYiBTZXJ2aWNlcyBPPUFtYXpvbi5jb20g
-SW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTI1MDIxODE5NDMw
-M1oXDTQ5MTIzMTIzNTk1OVowHjEcMBoGA1UEAwwTQVdTIElvVCBDZXJ0aWZpY2F0
-ZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBAMl0ETrsr7IQ2Hc3fyti
-xOCqsWifmRLiPsE8PGUlXiM4h5ux4dKOOa8ZH5s8JmNHW80vozEfFwc9GdxL77Aa
-SIYXUcxO8tU/xthADOZR2qKmJuOPQ5dl89aTso+e+dD7rK1cnmCuCE98UoeOqMtN
-fEo5d40zgTwZLJjzQflEMtNL6UVuuPW7CjeRju3ZKh30aEU+yii51siAgHUkc00T
-vCSUKEytDCS5zphYSYU9TkBj+nn0RiFZN2npptooZBiXFoL/RUSmxPks0cPp23R6
-FQwUATn8iR1cAZpopLkgDr9CMkmTnApKt2lFpK6qqoHH+Zh35qJXXSdm5uiI22nb
-RGsCAwEAAaNgMF4wHwYDVR0jBBgwFoAUY3ZiKGoC7f7MLKaAgd26cyOsYV8wHQYD
-VR0OBBYEFGOCZmowo7eqROIY3TUfPjtELsNLMAwGA1UdEwEB/wQCMAAwDgYDVR0P
-AQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQCoNwvxpQvKY7jVsoZq8favAd6F
-TW19dsy0k3RVGYMQQAxziIhrUfcq2X/bOxKutjegW9SGHlkX4V1/Wz3YcriU0/Mc
-sz83NYGbxR3Ojznvma0Wn8Wvz9yLo881QyqrVAyu+yAEywUcl979Z5pWuQqUj3us
-tHXIi2btyek2KzKOErOmmn4K621iXLgLc3jKqpsFeeN7qkSLgNTxfxBN1ZEo8nQk
-QSV3FDaJFT2eg89KL5p6kbENBmpXsLoFeKhWHYz8Irg5xM6pp4qdmk7RbJUTuO5C
-Xg8Yn/XlsIBL9xSi+x39zNRJyFUISi281+9X/8lKk3ojvxcwM9He3lUY9WaY
+SW5jLiBMPVNlYXR0bGUgU1Q9V2FzaGluZ3RvbiBDPVVTMB4XDTI1MDkxNTE5NDcx
+NVoXDTQ5MTIzMTIzNTk1OVowHjEcMBoGA1UEAwwTQVdTIElvVCBDZXJ0aWZpY2F0
+ZTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoCggEBANMJW+NHjf0jhFF9G8+f
+bkbN++CGQissyxMo3ZmDILwcYDuTg2lRTd7b3dXWfu9o728mANutdUFuEhdPP3FR
+7Tk37LQbw/qoLaussSeJYcR6uzdAUMfCsXU9Q2P/GLpq4vZpwNiz/CgkhgUC1DO8
+b/7dM1j1ODto4hr9P0KP6kvKETBpYGzWGXmGCtdVILtVGV4dtj4GIwlRxG//x8Yk
+8vsY10/NTgpvF+TDQpUGIMnrM9VZCFHFCt8vzc4lBdLJRemakqLtn9sYLu2H9EVo
+y31wwbpdMx/nfaRnp1Zy+V1rJhlC6sVwk+htaCSpEHdtWM8RiBjviYk4XNbNSOGs
+DX0CAwEAAaNgMF4wHwYDVR0jBBgwFoAUZsUwPW9e/4lxHLTrL72NcKlCEZswHQYD
+VR0OBBYEFCV4JZavrVqHFUcUImaHGlfwFLGPMAwGA1UdEwEB/wQCMAAwDgYDVR0P
+AQH/BAQDAgeAMA0GCSqGSIb3DQEBCwUAA4IBAQAzbB1ZgYZNio0JWguFfecs1MHm
+T0jhbNQYNwKFuoqE9w4rxPORPgw8C8hLrKyzC1LjloEzxjTlD3q0xT/Fnr5r6yig
+ozjofHnQBzKOQij1u4ks67cUX8wxHsZ3Kv9LtoR0G8aGAejGkEX24MsRvlE4xY/l
+QFlfNh9Ur3bevMeyZNyZwxN1iNVNKljP6Gp+7p81NI8jl95QOvfFWW0CdCwXmJhf
+Y2KBHuB5d3aAYlRIZrPO2PtN1k3I2UXN8wd87vbciGGm5AXlZ8KeeHWo7W7aPabN
+KR20ycWbse1CeEUxmOJfXepwxMgU27tc8jMreXRIa78OPXkPDLJnNiFeLefh
 -----END CERTIFICATE-----
 )EOF";
 
 const char AWS_CERT_PRIVATE[] = R"EOF(
 -----BEGIN RSA PRIVATE KEY-----
-MIIEpgIBAAKCAQEAyXQROuyvshDYdzd/K2LE4KqxaJ+ZEuI+wTw8ZSVeIziHm7Hh
-0o45rxkfmzwmY0dbzS+jMR8XBz0Z3EvvsBpIhhdRzE7y1T/G2EAM5lHaoqYm449D
-l2Xz1pOyj5750PusrVyeYK4IT3xSh46oy018Sjl3jTOBPBksmPNB+UQy00vpRW64
-9bsKN5GO7dkqHfRoRT7KKLnWyICAdSRzTRO8JJQoTK0MJLnOmFhJhT1OQGP6efRG
-IVk3aemm2ihkGJcWgv9FRKbE+SzRw+nbdHoVDBQBOfyJHVwBmmikuSAOv0IySZOc
-Ckq3aUWkrqqqgcf5mHfmolddJ2bm6IjbadtEawIDAQABAoIBAQCyqeCaHM3Zo70H
-Rk3foZIwGtKKknkE2WiEojKCFfj+TaVfPQylW4PnOeXhHCCDjgbp3CTkbMlUnyjv
-St2Bll3ZppbiFuagoBZvNab7IX9BHxCAY0+z1zTBim4jl//BC2PiwLCp188BB6Z+
-GNS0sbzYa00PCq3EKiN2Zs+NDaHx59y2abBAlNRRqj0PxLu97zHFlm508hA59szl
-zTHq4iyvdT/a90KDZ3ZMT3/YtHWUDO3yGDXsJIC/EK0JaIb6qGGfFlQgK9Sqtond
-xR9QjvP1m6PYcbA9ZTjcFAJrzt4j4zIxlMSR3LsiVg3mZtG5187dlvnHqZOATtNg
-H7hHnDyBAoGBAP7RFtj4NJ9A33iPbpLjIrt96vOHPc7ikUN0ryJWq+D6Cu2BgQjK
-XtuBBYePZQlhwTP1hGctCwvf3udBzjTCjQ1ozR/Zl0IpJCbRYSWwLp655h2uktON
-9H8zNEj+H+OFi13C+C9xVreoZvc6nkBBFcgfG/IFVQfnTpiagEUrSQfBAoGBAMpj
-ivxXMej37St/N5yB/IkzTmBuZjyjEOXRQV3yTgM/kC2ywM1dWbRVQ5RH5f327JXV
-tjAb9O6UP+ji8ZBx1GfcsBuw45PI9Ztt3PeCUJz1dD5a72dDCJMfAd9ONHsTKRgz
-giw9wfJk9zfW1lLKrAhf1gp5ZcWImX+pTVxTDLcrAoGBAKbq7F6AaGgRpNFik3og
-03WiDDAuXSySebmIS9jgkYK0CEiAVrgdaHm9UJP2OKcmo9qXb33uoZfLtEYkcyFY
-d/Fav1Onw0+Rh57t9QctG7zZ5YV3/7IZARn5vrw5y0XNCvgGaXEae0WfSKOWgGdy
-uwP2bOXi3EprGK8aNgAZVKOBAoGBAIq8U+EI8Kivk3/2tCtwG6n9ZqPJ6gF1ErdH
-i5SX7gxh/TamS247qKtqq2mxI6LFKobHuh0efPcy3uCI+Wi37w/Hg11oE4kR//Mt
-C4ULarE9vRKliWjy1aTsY38s/+LCTrBNVtV2/EVqGuHHPqIAVo9jCrmhYDdZnUea
-yfIY6CWDAoGBAJFBa+9L1ORf1mivb9+z+qj9pwKJV78UpTH/EEATlhL43LDjPWUo
-g1fZUCJtDRE4QbW2bYATuURTfugtDF1dJbIGLcbm9LA8Ydv0J9zzsH65tBmEqb2o
-mYFN+v2f6ri4+C7mXZTX49RLmA5OCaZyh0YK/onQ7hX7cUcJBdpMEDtH
+MIIEowIBAAKCAQEA0wlb40eN/SOEUX0bz59uRs374IZCKyzLEyjdmYMgvBxgO5OD
+aVFN3tvd1dZ+72jvbyYA2611QW4SF08/cVHtOTfstBvD+qgtq6yxJ4lhxHq7N0BQ
+x8KxdT1DY/8Yumri9mnA2LP8KCSGBQLUM7xv/t0zWPU4O2jiGv0/Qo/qS8oRMGlg
+bNYZeYYK11Ugu1UZXh22PgYjCVHEb//HxiTy+xjXT81OCm8X5MNClQYgyesz1VkI
+UcUK3y/NziUF0slF6ZqSou2f2xgu7Yf0RWjLfXDBul0zH+d9pGenVnL5XWsmGULq
+xXCT6G1oJKkQd21YzxGIGO+JiThc1s1I4awNfQIDAQABAoIBAEpOrVAlzdUoQFsB
+Fl3Q4V+E//9axsm1B/xDE2QOS+RwiWHw2wCmZCjoef/leuSJqmUUmJDBTtwvCaez
+e79G/aR8EJiKfkMwHISqVeZ59TbdkDYwK0FrSFjjrWr0U5ywLiJdsk2bclogIiB5
+koZ0/2paMM5N6fLhR1wBd9jUG8d7RpO/rbACgFLihunAqnCt2hiZKEBvZROteWeq
+fLShZvGwK6BanUDs8PgAe8oZUrGehkmZKoL3zv5pRP/XWrrtAq+F6u4jPOJ7Sy3J
+4Z5l5nT4WV1AgmG+i+rj0+8M7XkUsTyVro4LI1A7PO2Uuv+hYgMmkqH6UKzv5Wip
+cdArsoECgYEA/zHxlihCWbYd84qtjATRulXlBtQv3GFOJZQ+tIv6eY1qV7amaJBe
+oF+Cphi2kYnzFx+6ElQdNYML3nCDUwS7PWIFwbtTg/f0BIJVj4igbCPmWM1d6lal
+J2nvcicCGXelgCEg3HaZoCeJhO2T8ws7r2FrkNK9TNkEnqcycbyefskCgYEA07PC
+dR2Rv51jz6FqbgwcCQGHcQx5yBB9PZ6x+8M3nTRyk9PV2YAp6svtAskzVAVWDicu
+0uN+MBUXyGRfavsN1oNQijbrrmUR0gLyrWu298pxth7l/pxAodPgi/N7B9tz+O/C
+FQL723Fi1PYwC1BHHwQ04NvRjwMtXwoe347e7xUCgYANSzbAkbxTHA+XXkQIJUAf
+qtXRc0BDBLajcoOJuDkGF/3QL+2fSNYFGLSfnm30SvFk5Pw2dDdJUFhP8CCHNJnk
+arTOEMoa99G4Ymm+nmls+LnJKnmX+YeE9BnLIokTdo4ijJc/pNX/589cI1wvrwge
+7L5DvxapaCy7bw5Lqn2/MQKBgGUtsrB6k1/6/usLAGzcCMIdIvhauBFwIAtFP1dO
+M9REbsZMJhavdFktYXdFZeywKDLXcz+DTpn4WseBgUEF/9UYKXQFT5qn31553jYY
+FvhSnwoltlJ7p0AwnU8F7TUSI1lEbYPRS0HT7SJPDyM5GOSwA/270WoCYLrdgWfj
+RpUJAoGBAO6pFAirDe+6ubspZ7ABNKLu6N4VnEfCf1Q/o1aS4wr1C+7Z9nnH6kPC
+i8aizEQg1IEKM7YO/pvYwi6mDlh40Hk6Jmix2XK0AfwSC7BycSa69RNH0fH9vqPb
+KELkivA1Lh8M8BCOuPyH6ekYUxK601p2Gf/4o/QMkMxLfhnXU4FV
 -----END RSA PRIVATE KEY-----
 )EOF";
 
+// This is Oldd
 // void connectWiFi() {
 //   Serial.print("Connecting to WiFi...");
 //   WiFi.begin(ssid, password);
@@ -121,6 +123,7 @@ mYFN+v2f6ri4+C7mXZTX49RLmA5OCaZyh0YK/onQ7hX7cUcJBdpMEDtH
 //   Serial.println("\nWiFi Connected!");
 // }
 
+//This works
 void connectWiFi() {
   WiFi.mode(WIFI_STA);
   WiFiManager wm;
@@ -168,6 +171,32 @@ void connectWiFi() {
     Serial.println("\nWiFi Connected with saved credentials.");
   }
 }
+
+//New for configuring wifi every time
+// void connectWiFi() {
+//   WiFi.mode(WIFI_STA);
+
+//   // (Optional) ensure nothing auto-reconnects before we show the portal
+//   WiFi.disconnect(true, true);     // erase old STA config & stop WiFi
+//   delay(100);
+//   WiFi.persistent(false);          // don't auto-save creds silently
+
+//   WiFiManager wm;
+
+//   // Optional: make the portal exit after N seconds if no config is done
+//   // wm.setConfigPortalTimeout(180); // 3 minutes
+
+//   Serial.println("\nStarting WiFi config portal (forced)...");
+//   // This ALWAYS starts the AP + captive portal, *without* trying saved APs first
+//   bool ok = wm.startConfigPortal("RehabGait_WifiConfig"); // , "optionalAPpass"
+
+//   if (ok) {
+//     Serial.println("WiFi Connected via portal.");
+//   } else {
+//     Serial.println("No config done / failed. Restarting...");
+//     ESP.restart();
+//   }
+// }
 
 
 void connectAWS() {
